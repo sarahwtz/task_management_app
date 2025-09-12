a7103429a3ebd608ee335e21c8f5761fba719394
sarahwtz <sarahwtz@gmail.com>
2025-09-12 11:59:22 +0200

    feat(auth): add middleware and auth check to TarefaController

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +18,16 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        //
+       
+        $user = auth()->user();
+
+       if (!$user) {
+             return 'Você não está logado no sistema';
+      }
+
+             return "ID: | {$user->id} | Nome: {$user->name} | Email: {$user->email}";
+             
+
     }
 
     /**
